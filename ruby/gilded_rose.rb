e3dbae36e7bb8bbfe12e27948dbe30a94b12f9b2
Ishwar <ishwar.khetpal@protonshub.in>
2023-12-19 11:01:23 +0530

    Minor code changes for update quality method

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -15,23 +15,22 @@ class GildedRose
       else
         if item.quality < 50
           item.quality = item.quality + 1
+
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+            if item.sell_in < 11 and item.quality < 50
+              item.quality = item.quality + 1
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+            if item.sell_in < 6 and item.quality < 50
+              item.quality = item.quality + 1
             end
           end
         end
       end
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
